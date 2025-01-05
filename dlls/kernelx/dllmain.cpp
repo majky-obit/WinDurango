5e97f7d43048d1231f4dbea5e540cac29b099042
Rodrigo Todescatto <90097545+Rodrigo-Todescatto@users.noreply.github.com>
2025-01-05 14:49:15 -0300

    Added Forza debbuging and fixed XMemSetAllocationHooks.

--- dlls/kernelx/dllmain.cpp
+++ dlls/kernelx/dllmain.cpp
@@ -1,11 +1,21 @@
 #include "pch.h"
 #include "hooks.h"
+#include "ForzaThreadHook_X.h"
+#include "kernelx.h"
 
 // note from unixian: i used this since using appxlauncher requires me attaching to the game after it launches
 #define WINDURANGO_WAIT_FOR_DEBUGGER 0
 
+//Rodrigo Todescatto: For debbuging Forza.
+#define RETURN_IF_FAILED(hr) if (FAILED(hr)) return hr
+#define FORZADEBUG
+
 BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID reserved)
 {
+	winrt::hstring GamePackage = winrt::Windows::ApplicationModel::Package::Current().Id().FamilyName();
+
+	InitializeCriticalSection(&XMemSetAllocationHooksLock_X);
+
 	if (DetourIsHelperProcess()) return TRUE;
 
 	if (dwReason == DLL_PROCESS_ATTACH)
@@ -19,6 +29,65 @@ BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID reserved)
 			freopen_s(&stream, "CONOUT$", "w", stdout);
 			SetConsoleTitleA("WinDurango Debug Console");
 		}
+		
+#ifdef FORZADEBUG
+		//Rodrigo Todescatto: Forza Horizon 2 Demo.
+		if (GamePackage == L"265E1020-Anthem_8wekyb3d8bbwe")
+		{
+			printf("Forza Horizon 2 Demo\n");
+			*(void**)&P_StartForzaThread_X = (char*)GetModuleHandleW(nullptr) + 0xFE6920;
+			if (FAILED(P_StartForzaThread_X))
+			{
+				printf("P_StartForzaThread_X failed!\n");
+				OutputDebugStringW(L"P_StartForzaThread_X failed!\n");
+			}
+			if (SUCCEEDED(P_StartForzaThread_X))
+			{
+				printf("P_StartForzaThread_X succeeded!\n");
+				OutputDebugStringW(L"P_StartForzaThread_X succeeded!\n");
+			}
+			RETURN_IF_FAILED(HRESULT_FROM_WIN32(DetourAttach((void**)&P_StartForzaThread_X, &D_StartForzaThread_X)));
+		}
+		//Rodrigo Todescatto: Forza Horizon 2.
+		if (GamePackage == L"Anthem_8wekyb3d8bbwe")
+		{
+			printf("Forza Horizon 2\n");
+			*(void**)&P_StartForzaThread_X = (char*)GetModuleHandleW(nullptr) + 0x1081A90;
+			if (FAILED(P_StartForzaThread_X))
+			{
+				printf("P_StartForzaThread_X failed!\n");
+				OutputDebugStringW(L"P_StartForzaThread_X failed!\n");
+			}
+			if (SUCCEEDED(P_StartForzaThread_X))
+			{
+				printf("P_StartForzaThread_X succeeded!\n");
+				OutputDebugStringW(L"P_StartForzaThread_X succeeded!\n");
+			}
+			*(void**)&P_FmodThreadProc_X = (char*)GetModuleHandleW(nullptr) + 0x19D3F80;
+			if (FAILED(P_FmodThreadProc_X))
+			{
+				printf("P_FmodThreadProc_X failed!\n");
+				OutputDebugStringW(L"P_FmodThreadProc_X failed!\n");
+			}
+			if (SUCCEEDED(P_FmodThreadProc_X))
+			{
+				printf("P_FmodThreadProc_X succeeded!\n");
+				OutputDebugStringW(L"P_FmodThreadProc_X succeeded!\n");
+			}
+			RETURN_IF_FAILED(HRESULT_FROM_WIN32(DetourAttach((void**)&P_StartForzaThread_X, &D_StartForzaThread_X)));
+			RETURN_IF_FAILED(HRESULT_FROM_WIN32(DetourAttach((void**)&P_FmodThreadProc_X, &D_FmodThreadProc_X)));
+		}
+		//Rodrigo Todescatto: Forza Motorsport 5.
+		if (GamePackage == L"Forza_8wekyb3d8bbwe")
+		{
+			printf("Forza Motorsport 5");
+		}
+		//Rodrigo Todescatto: Forza Horizon 2 Presents Fast & Furious.
+		if (GamePackage == L"Spire_8wekyb3d8bbwe")
+		{
+			printf("Forza Horizon 2 Presents Fast & Furious");
+		}
+#endif
 
 #if WINDURANGO_WAIT_FOR_DEBUGGER
 		printf("Waiting for debugger...\n");
--- dlls/kernelx/utils.h
+++ dlls/kernelx/utils.h
@@ -13,28 +13,7 @@
 
 BOOL IsXboxModule(HMODULE module)
 {
-	wchar_t moduleFilePath[MAX_PATH];
-	if (GetModuleFileNameW(module, moduleFilePath, MAX_PATH) > 0)
-	{
-		std::wstring moduleFileName(moduleFilePath);
-		wprintf(L"%ls\n", moduleFileName.c_str());
-
-		wchar_t exeFilePath[MAX_PATH];
-		if (GetModuleFileNameW(NULL, exeFilePath, MAX_PATH) > 0)
-		{
-			std::wstring exeDir(exeFilePath);
-			size_t pos = exeDir.find_last_of(L"\\/");
-			if (pos != std::wstring::npos) {
-				exeDir = exeDir.substr(0, pos);
-			}
-
-			if (moduleFileName.find(exeDir) == 0) {
-				return TRUE;
-			}
-		}
-	}
-
-	return FALSE;
+	return module == GetModuleHandleW(nullptr);
 }
 
 inline BOOL IsXboxAddress(const PVOID Address)
