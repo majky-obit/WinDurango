3b2197d493df332d080e9af3e233b602be775cd4
Unixian <48267798+notunixian@users.noreply.github.com>
2025-01-16 00:39:04 -0500

    fix stats crash + remove oopsie from storage

--- dlls/winrt_x/Implementation/Microsoft.Xbox.Services.UserStatistics.UserStatisticsService.cpp
+++ dlls/winrt_x/Implementation/Microsoft.Xbox.Services.UserStatistics.UserStatisticsService.cpp
@@ -41,7 +41,11 @@ namespace winrt::Microsoft::Xbox::Services::UserStatistics::implementation
     }
     winrt::Windows::Foundation::IAsyncOperation<winrt::Windows::Foundation::Collections::IVectorView<winrt::Microsoft::Xbox::Services::UserStatistics::UserStatisticsResult>> UserStatisticsService::GetMultipleUserStatisticsAsync(winrt::Windows::Foundation::Collections::IVectorView<hstring> xboxUserIds, hstring serviceConfigurationId, winrt::Windows::Foundation::Collections::IVectorView<hstring> statisticNames)
     {
-        throw hresult_not_implemented();
+		for (uint32_t i = 0; i < statisticNames.Size( ); i++) {
+			printf("[UserStatisticsService] GetMultipleUserStatisticsAsync [Statistic (%u)] [%ls]\n", i, statisticNames.GetAt(i).c_str( ));
+		}
+
+		co_return winrt::single_threaded_vector<UserStatisticsResult>( ).GetView( );
     }
     winrt::Windows::Foundation::IAsyncOperation<winrt::Windows::Foundation::Collections::IVectorView<winrt::Microsoft::Xbox::Services::UserStatistics::UserStatisticsResult>> UserStatisticsService::GetMultipleUserStatisticsForMultipleServiceConfigurationsAsync(winrt::Windows::Foundation::Collections::IVectorView<hstring> xboxUserIds, winrt::Windows::Foundation::Collections::IVectorView<winrt::Microsoft::Xbox::Services::UserStatistics::RequestedStatistics> requestedServiceConfigurationStatisticsCollection)
     {
