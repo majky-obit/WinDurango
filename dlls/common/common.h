94749fe1250b018584bf2bb821a058a528fb34ac
Serenity <34622204+oSerenity@users.noreply.github.com>
2025-02-18 13:09:35 -0500

    D3DMapEsramMemory_X Implementation

--- dlls/common/common.h
+++ dlls/common/common.h
@@ -1,1 +1,5 @@
 ﻿#define DurangoAPI      __stdcall
+#define RtlSetLastWin32ErrorAndNtStatusFromNtStatus(Status) \
+    SetLastError(RtlNtStatusToDosError(Status))
+#define RtlGetLastWin32ErrorAndNtStatusFromNtStatus(Status) \
+    GetLastError(RtlNtStatusToDosError(Status))
