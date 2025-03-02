b9cbc04ce13604982489588166a3b68bdb825415
Unixian <48267798+notunixian@users.noreply.github.com>
2025-03-02 18:16:14 -0500

    udpate detours submodule

--- symlink.ps1
+++ symlink.ps1
@@ -1,4 +1,4 @@
-$game_path = "C:\\Users\\unixian\\Desktop\\reversing\\xb1\\minecraft_lce_xb1\\Mount"
+$game_path = "C:\\Users\\unixian\\Desktop\\reversing\\xb1\\A5790400-8451-4654-824A-5096FC7E7F09_1.0.0.8_x64__c74r4999cqbdr\\Mount"
 $out_dll_dir = "C:\\Users\\unixian\\Documents\\GitHub\\WinDurango\\x64\\Debug"
 
 function make-link ($dll_name) {
@@ -17,6 +17,6 @@ make-link("kernelx.dll")
 make-link("mfplat.dll")
 make-link("MMDevAPI.dll")
 make-link("XboxIntegratedMultiplayer.dll")
-make-link("xg_x.dll")
+#make-link("xg_x.dll")
 make-link("XFrontPanelDisplay.dll")
 make-link("XAudio2_9_x.dll")
