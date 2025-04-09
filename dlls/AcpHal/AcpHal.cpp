c217b17b0bde52b6af3719c9c05d94b70a500bf0
Tyler Jaacks <TylerJaacks@Gmail.com>
2025-04-08 23:03:18 -0500

    Tabified.

--- dlls/AcpHal/AcpHal.cpp
+++ dlls/AcpHal/AcpHal.cpp
@@ -9,114 +9,114 @@
 #include "contexts.h"
 
 struct SHAPE_CONTEXTS {
-    UINT32 numSrcContexts;
-    UINT32 numEqCompContexts;
-    UINT32 numFiltVolContexts;
-    UINT32 numDmaContexts;
-    UINT32 numXmaContexts;
-    UINT32 numPcmContexts;
-    SHAPE_SRC_CONTEXT* srcContextArray;
-    SHAPE_EQCOMP_CONTEXT* eqCompContextArray;
-    SHAPE_FILTVOL_CONTEXT* filtVolContextArray;
-    SHAPE_DMA_CONTEXT* dmaContextArray;
-    SHAPE_XMA_CONTEXT* xmaContextArray;
-    SHAPE_PCM_CONTEXT* pcmContextArray;
-    APU_ADDRESS apuSrcContextArray;
-    APU_ADDRESS apuEqCompContextArray;
-    APU_ADDRESS apuFiltVolContextArray;
-    APU_ADDRESS apuDmaContextArray;
-    APU_ADDRESS apuXmaContextArray;
-    APU_ADDRESS apuPcmContextArray;
+	UINT32 numSrcContexts;
+	UINT32 numEqCompContexts;
+	UINT32 numFiltVolContexts;
+	UINT32 numDmaContexts;
+	UINT32 numXmaContexts;
+	UINT32 numPcmContexts;
+	SHAPE_SRC_CONTEXT* srcContextArray;
+	SHAPE_EQCOMP_CONTEXT* eqCompContextArray;
+	SHAPE_FILTVOL_CONTEXT* filtVolContextArray;
+	SHAPE_DMA_CONTEXT* dmaContextArray;
+	SHAPE_XMA_CONTEXT* xmaContextArray;
+	SHAPE_PCM_CONTEXT* pcmContextArray;
+	APU_ADDRESS apuSrcContextArray;
+	APU_ADDRESS apuEqCompContextArray;
+	APU_ADDRESS apuFiltVolContextArray;
+	APU_ADDRESS apuDmaContextArray;
+	APU_ADDRESS apuXmaContextArray;
+	APU_ADDRESS apuPcmContextArray;
 };
 
 
 HRESULT AcpHalAllocateShapeContexts_X(SHAPE_CONTEXTS* ctx) {
-    if (ctx->numSrcContexts > 0)
-        ctx->srcContextArray = static_cast<SHAPE_SRC_CONTEXT*>(malloc(sizeof(SHAPE_SRC_CONTEXT) * ctx->numSrcContexts));
+	if (ctx->numSrcContexts > 0)
+		ctx->srcContextArray = static_cast<SHAPE_SRC_CONTEXT*>(malloc(sizeof(SHAPE_SRC_CONTEXT) * ctx->numSrcContexts));
 
-    if (ctx->numEqCompContexts > 0)
-        ctx->eqCompContextArray = static_cast<SHAPE_EQCOMP_CONTEXT*>(malloc(sizeof(SHAPE_EQCOMP_CONTEXT) * ctx->numEqCompContexts));
+	if (ctx->numEqCompContexts > 0)
+		ctx->eqCompContextArray = static_cast<SHAPE_EQCOMP_CONTEXT*>(malloc(sizeof(SHAPE_EQCOMP_CONTEXT) * ctx->numEqCompContexts));
 
-    if (ctx->numFiltVolContexts > 0)
-        ctx->filtVolContextArray = static_cast<SHAPE_FILTVOL_CONTEXT*>(malloc(sizeof(SHAPE_FILTVOL_CONTEXT) * ctx->numFiltVolContexts));
+	if (ctx->numFiltVolContexts > 0)
+		ctx->filtVolContextArray = static_cast<SHAPE_FILTVOL_CONTEXT*>(malloc(sizeof(SHAPE_FILTVOL_CONTEXT) * ctx->numFiltVolContexts));
 
-    if (ctx->numDmaContexts > 0)
-        ctx->dmaContextArray = static_cast<SHAPE_DMA_CONTEXT*>(malloc(sizeof(SHAPE_DMA_CONTEXT) * ctx->numDmaContexts));
+	if (ctx->numDmaContexts > 0)
+		ctx->dmaContextArray = static_cast<SHAPE_DMA_CONTEXT*>(malloc(sizeof(SHAPE_DMA_CONTEXT) * ctx->numDmaContexts));
 
-    if (ctx->numXmaContexts > 0)
-        ctx->xmaContextArray = static_cast<SHAPE_XMA_CONTEXT*>(malloc(sizeof(SHAPE_XMA_CONTEXT) * ctx->numXmaContexts));
+	if (ctx->numXmaContexts > 0)
+		ctx->xmaContextArray = static_cast<SHAPE_XMA_CONTEXT*>(malloc(sizeof(SHAPE_XMA_CONTEXT) * ctx->numXmaContexts));
 
-    if (ctx->numPcmContexts > 0)
-        ctx->pcmContextArray = static_cast<SHAPE_PCM_CONTEXT*>(malloc(sizeof(SHAPE_PCM_CONTEXT) * ctx->numPcmContexts));
+	if (ctx->numPcmContexts > 0)
+		ctx->pcmContextArray = static_cast<SHAPE_PCM_CONTEXT*>(malloc(sizeof(SHAPE_PCM_CONTEXT) * ctx->numPcmContexts));
 
-    printf("[AcpHal] allocated shape contexts\n");
-    return S_OK;
+	printf("[AcpHal] allocated shape contexts\n");
+	return S_OK;
 }
 
 HRESULT AcpHalReleaseShapeContexts_X() {
-    DEBUG_PRINT();
+	DEBUG_PRINT();
 	return S_OK;
 }
 
 HRESULT AcpHalCreate_X(IAcpHal** acpInterface) {
 	printf("[WARNING] AcpHalCreate returns back a nullptr, the game is likely to crash!\n");
 	*acpInterface = nullptr;
-    return 0;
+	return 0;
 }
 
 HRESULT ApuAlloc_X(
-         void** virtualAddress,
-         APU_ADDRESS* physicalAddress,
-         UINT32 sizeInBytes,
-         UINT32 alignmentInBytes,
-         UINT32 flags
+		 void** virtualAddress,
+		 APU_ADDRESS* physicalAddress,
+		 UINT32 sizeInBytes,
+		 UINT32 alignmentInBytes,
+		 UINT32 flags
 )
 {
-    alignmentInBytes = 4;
-    DEBUG_PRINT();
-    return 0;
+	alignmentInBytes = 4;
+	DEBUG_PRINT();
+	return 0;
 }
 
 HRESULT ApuCreateHeap_X(size_t initialSize, size_t maximumSize) {
-    // Don't think we need that as this is for chaning memory pool iirc...
-    return 0;
+	// Don't think we need that as this is for chaning memory pool iirc...
+	return 0;
 }
 
 HRESULT ApuFree_X(void* virtualAddress) {
-    DEBUG_PRINT();
-    free(virtualAddress);
-    return 0;
+	DEBUG_PRINT();
+	free(virtualAddress);
+	return 0;
 }
 
 HRESULT ApuHeapGetState_X(
-         ApuHeapState* apuHeapState,
-         UINT32 flags
+		 ApuHeapState* apuHeapState,
+		 UINT32 flags
 )
 {
-    DEBUG_PRINT();
-    return 0;
+	DEBUG_PRINT();
+	return 0;
 }
 
 bool ApuIsVirtualAddressValid_X(
-         const void* virtualAddress,
-         UINT32 physicalAlignmentInBytes
+		 const void* virtualAddress,
+		 UINT32 physicalAlignmentInBytes
 )
 {
-    DEBUG_PRINT();
-    return 0;
+	DEBUG_PRINT();
+	return 0;
 }
 APU_ADDRESS ApuMapVirtualAddress_X(
-         const void* virtualAddress
+		 const void* virtualAddress
 )
 {
-    DEBUG_PRINT();
-    return 0;
+	DEBUG_PRINT();
+	return 0;
 }
 
 void* ApuMapApuAddress_X(
-         APU_ADDRESS apuPhysicalAddress
+		 APU_ADDRESS apuPhysicalAddress
 )
 {
-    DEBUG_PRINT();
-    return 0;
+	DEBUG_PRINT();
+	return 0;
 }
