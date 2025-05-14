edfd1ed32bdaa1399a8c1faf381d58f3d431fed5
Serenity <34622204+oSerenity@users.noreply.github.com>
2025-05-14 01:07:06 -0400

    Gamepad Support
    allows virtual keyboard to use gamepad

--- dlls/d3d11_x/overlay/overlay.h
+++ dlls/d3d11_x/overlay/overlay.h
@@ -1,6 +1,10 @@
 #pragma once
 #include <d3d11.h>
 #include <dxgi1_2.h>
+#include <imgui.h>
+#include <winrt/Windows.UI.Core.h>
+#include <winrt/Windows.System.h>
+#include <winrt/Windows.Foundation.h>
 
 namespace wd
 {
@@ -9,10 +13,19 @@ namespace wd
 	public:
 		Overlay(ID3D11Device* pDevice, ID3D11DeviceContext* pContext, IDXGISwapChain1* pSwapchain);
 		~Overlay( );
-		void Initialize();
+		void Initialize( );
 		void Shutdown( );
 		void EnableKeyboard( );
+		void UpdateXInput( );
 		void Present( );
+
+		void RenderKeyboardWindow( );
+
+		void RenderKeyboardRow(const char** keys, int start, int end, bool isUppercase);
+
+		void AddKeyToBuffer(char c);
+
+		void HandleKeyboardSpecialKeys( );
 
 	private:
 		bool m_bOpen = false;
